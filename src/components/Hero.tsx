a524cf1dff4ac7c70017342391a8d0baef45d326
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2026-04-29 11:48:49 -0500

    Set up HTMl for Hero component and added some styles for the image and picture and the h1.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,27 @@
 import "./Hero.css";
+import heroImage from "../assets/images/image-web-3-mobile.jpg";
+import heroImageDesktop from "../assets/images/image-web-3-desktop.jpg";
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <h1>Hello world</h1>
-    </div>
+    <article className="hero">
+      <picture>
+        <source media="(min-width: 768px)" srcSet={heroImageDesktop} />
+        <img src={heroImage} alt="colorful blocks" />
+      </picture>
+
+      <div className="hero__content">
+        <h1>The Bright Future of Web 3.0?</h1>
+        <div className="hero__description">
+          <p>
+            We dive into the next evolution of the web that claims to put the
+            power of the platforms back into the hands of the people. But is it
+            really fulfilling its promise?
+          </p>
+          <button>READ MORE</button>
+        </div>
+      </div>
+    </article>
   );
 };
 
