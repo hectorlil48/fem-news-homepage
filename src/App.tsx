ede27405ee0adaeeb274cb2c4e5ed8f5cf60fa3b
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2026-04-29 09:24:10 -0500

    Added a brigger width for the mobile menu on the break point. Also added a useEffect to close the mobile menu if the screen sizes gets bigger.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Nav from "./components/Nav";
 import MobileMenu from "./components/MobileMenu";
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1024) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
       <Nav isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
