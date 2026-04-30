cbff048a2e298869460a605f9692c921d0698bbc
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2026-04-29 20:35:17 -0500

    Send props from app to article card. to use the data to make the card reusable.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,31 @@ import Nav from "./components/Nav";
 import MobileMenu from "./components/MobileMenu";
 import Hero from "./components/Hero";
 import NewsSidebar from "./components/NewsSidebar";
+import retroPCs from "./assets/images/image-retro-pcs.jpg";
+import laptops from "./assets/images/image-top-laptops.jpg";
+import gaming from "./assets/images/image-gaming-growth.jpg";
+import ArticleCard from "./components/ArticleCard";
+
+const articles = [
+  {
+    number: "01",
+    title: "Reviving Retro PCs",
+    description: "What happens when old PCs are given modern upgrades?",
+    image: retroPCs,
+  },
+  {
+    number: "02",
+    title: "Top 10 Laptops of 2022",
+    description: "Our best picks for various needs and budgets.",
+    image: laptops,
+  },
+  {
+    number: "03",
+    title: "The Growth of Gaming",
+    description: "How the pandemic has sparked fresh opportunities.",
+    image: gaming,
+  },
+];
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
@@ -26,10 +51,16 @@ function App() {
       <Nav isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <MobileMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <main>
-        <div className="main__grid container">
+        <section className="main__grid container">
           <Hero />
           <NewsSidebar />
-        </div>
+        </section>
+
+        <section>
+          {articles.map((article) => (
+            <ArticleCard key={article.number} {...article} />
+          ))}
+        </section>
       </main>
     </>
   );
--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -1,5 +1,26 @@
-const ArticleCard = () => {
-  return <div></div>;
+type ArticleCardProps = {
+  number: string;
+  title: string;
+  description: string;
+  image: string;
+};
+
+const ArticleCard = ({
+  number,
+  title,
+  description,
+  image,
+}: ArticleCardProps) => {
+  return (
+    <article className="article">
+      <img src={image} alt={title} className="article__image" />
+      <div className="article__content">
+        <span className="article__number">{number}</span>
+        <h3 className="article__title">{title}</h3>
+        <p className="article__description">{description}</p>
+      </div>
+    </article>
+  );
 };
 
 export default ArticleCard;
